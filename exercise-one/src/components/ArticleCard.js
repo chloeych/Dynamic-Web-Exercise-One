05a19ca52f3451bc8316c7b7405d97441fdf47ff
chloeych <52624099+chloeych@users.noreply.github.com>
2020-02-20 16:04:12 -0500

    first commit

---
+++ exercise-one/src/components/ArticleCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ArticleCard({ articleData }){
+  // console.log('article card articleData', article);
+  return(
+    <article className="articleCard">
+      <div className="articleCard__image">
+        <img src={articleData.image.url} alt={articleData.image.alt} />
+      </div>
+
+      <div className="articleCard__text">
+        <h2>{articleData.title}</h2>
+        <p>{articleData.publishedDate}</p>
+        <p>{articleData.blurb}</p>
+        <a href= {`article/${articleData.id}`}> Read More</a>
+      </div>
+    </article>
+  )
+}
+
+export default ArticleCard;
